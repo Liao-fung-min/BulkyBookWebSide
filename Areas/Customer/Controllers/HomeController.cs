af217591260e4a73090f52cbc4b0f50542094458
Alex <rice0037308@gmail.com>
2022-08-13 01:25:41 +0800

    新增shoppingcart model

--- Areas/Customer/Controllers/HomeController.cs
+++ Areas/Customer/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -23,6 +24,17 @@ namespace BulkyBookWeb.Controllers
             IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
 
             return View(productList);
+        }
+        public IActionResult Details(int id)
+        {
+            ShoppingCart CartObj = new()
+            {
+                Count = 1,
+                Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id, includeProperties: "Category,CoverType")
+
+
+            };
+            return View(CartObj);
         }
 
         public IActionResult Privacy()
